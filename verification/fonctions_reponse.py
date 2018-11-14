b24c147175f5f58e1a9d34f2eb9a464b218bb3ca
Jonathan Julou <43301289+jonathanjulou@users.noreply.github.com>
2018-11-14 20:22:00 +0100

    Add files via upload

---
+++ verification/fonctions_reponse.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 20:17:05 2018
+
+@author: Admin
+"""
+
+import json
+from gestionnaire_bloc import *
+from listener import *
+
+def ans_inv(message):
+    
+def ans_notfound(message):
+    
+def ans_getdata(message):
